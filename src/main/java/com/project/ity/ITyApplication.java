116b7557c488bc8be4ae78c42523a7636b210854
jiyoungmerong <jiyoungmerong@gmail.com>
2024-06-30 15:03:51 +0900

    [Setting] Create project File and connect to DB

---
+++ src/main/java/com/project/ity/ITyApplication.java
@@ -0,0 +1,13 @@
+package com.project.ity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ITyApplication.class, args);
+    }
+
+}
