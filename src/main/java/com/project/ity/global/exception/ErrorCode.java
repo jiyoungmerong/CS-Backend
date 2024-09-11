8f44a3a7d42c8a0e972c9b63286795840e103ec5
jiyoungmerong <jiyoungmerong@gmail.com>
2024-09-11 20:48:12 +0900

    [Feat] Add error management DTO

--- src/main/java/com/project/ity/global/exception/ErrorCode.java
+++ src/main/java/com/project/ity/global/exception/ErrorCode.java
@@ -5,6 +5,28 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
+    HTTP_MESSAGE_NOT_READABLE(400, "요청값을 읽어들이지 못했습니다. 형식을 확인해 주세요."),
+
+    // 인증 - 토큰
+    NOT_EXISTS_AUTH_HEADER(401, "Authorization Header가 빈 값입니다."),
+    NOT_VALID_BEARER_GRANT_TYPE(401, "인증 타입이 Bearer 타입이 아닙니다."),
+    TOKEN_EXPIRED(401, "해당 token은 만료되었습니다."),
+    ACCESS_TOKEN_EXPIRED(401, "해당 access token은 만료되었습니다."),
+    NOT_ACCESS_TOKEN_TYPE(401, "tokenType이 access token이 아닙니다."),
+    REFRESH_TOKEN_EXPIRED(401, "해당 refresh token은 만료되었습니다."),
+    REFRESH_TOKEN_NOT_FOUND(400, "해당 refresh token은 존재하지 않습니다."),
+    NOT_VALID_TOKEN(401, "유효하지 않은 토큰입니다."),
+    NO_ACCESS_USER(403, "권한이 없습니다."),
+
+    // 인증 - 로그인 시도
+    MISMATCHED_SIGNIN_INFO(400, "잘못된 로그인 정보입니다."),
+
+    // 회원가입 시 중복 체크
+    ID_ALREADY_REGISTERED(409, "이미 가입된 아이디입니다."),
+    NUMBER_ALREADY_REGISTERED(409, "이미 가입된 번호입니다."),
+    NICKNAME_ALREADY_REGISTERED(409, "이미 가입된 닉네임입니다."),
+
+    USER_NOT_FOUND(404, "해당 사용자가 존재하지 않습니다."),
     ;
 
     private final int statusCode;
