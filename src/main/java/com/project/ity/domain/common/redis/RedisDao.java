d34cc9051dad8f1ea15b00e9010ea8455358822d
jiyoungmerong <jiyoungmerong@gmail.com>
2024-10-07 22:33:29 +0900

    [Feat] Add redis settings

---
+++ src/main/java/com/project/ity/domain/common/redis/RedisDao.java
@@ -0,0 +1,44 @@
+package com.project.ity.domain.common.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class RedisDao {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void setValues(String key, String data) {
+        ValueOperations<String, Object> values = redisTemplate.opsForValue();
+        values.set(key, data);
+    }
+
+    public void setValuesList(String key, String data) {
+        redisTemplate.opsForList().rightPushAll(key,data);
+    }
+
+    public List<Object> getValuesList(String key) {
+        Long len = redisTemplate.opsForList().size(key);
+        return len == 0 ? new ArrayList<>() : redisTemplate.opsForList().range(key, 0, len-1);
+    }
+
+    public void setValues(String key, String data, Duration duration) {
+        ValueOperations<String, Object> values = redisTemplate.opsForValue();
+        values.set(key, data, duration);
+    }
+
+    public Object getValues(String key) {
+        ValueOperations<String, Object> values = redisTemplate.opsForValue();
+        return values.get(key);
+    }
+
+    public void deleteValues(String key) {
+        redisTemplate.delete(key);
+    }
+}
