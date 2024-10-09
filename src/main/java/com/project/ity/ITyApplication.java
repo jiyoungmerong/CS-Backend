d205300f221299a0d0da659712cc13156b5f7bb3
jiyoungmerong <jiyoungmerong@gmail.com>
2024-10-09 23:05:14 +0900

    [Feat] Add Cache-applied annotations

--- src/main/java/com/project/ity/ITyApplication.java
+++ src/main/java/com/project/ity/ITyApplication.java
@@ -2,12 +2,14 @@ package com.project.ity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAspectJAutoProxy
+@EnableCaching
 public class ITyApplication {
 
     public static void main(String[] args) {
