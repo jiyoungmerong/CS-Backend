3f1651c9af06d82d2c08016de04efc9221c1a139
jiyoungmerong <jiyoungmerong@gmail.com>
2024-10-24 20:51:08 +0900

    [Change] Modify TokenManager and Add Redis settings

--- src/main/java/com/project/ity/domain/jwt/service/TokenManager.java
+++ src/main/java/com/project/ity/domain/jwt/service/TokenManager.java
@@ -4,7 +4,9 @@ import com.project.ity.domain.jwt.constant.AuthScheme;
 import com.project.ity.domain.jwt.constant.TokenType;
 import com.project.ity.domain.jwt.dto.TokenDto;
 import com.project.ity.global.exception.ErrorCode;
+import com.project.ity.global.exception.exceptions.BusinessException;
 import com.project.ity.global.exception.exceptions.JwtAuthException;
+import com.project.ity.global.util.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,12 +26,14 @@ public class TokenManager {
     private final long refreshTokenExpMillis;
     private final Key key;
 
+    private final RedisUtil redisUtil;
+
     @Autowired
     public TokenManager(
             @Value("${jwt.secret}") String tokenSecret
             , @Value("${jwt.access-token-expiration-time}") long accessTokenExpMillis
-            , @Value("${jwt.refresh-token-expiration-time}") long refreshTokenExpMillis) {
-
+            , @Value("${jwt.refresh-token-expiration-time}") long refreshTokenExpMillis, RedisUtil redisUtil) {
+        this.redisUtil = redisUtil;
         byte[] keyBytes = Decoders.BASE64.decode(tokenSecret);
         this.key = Keys.hmacShaKeyFor(keyBytes);
         this.accessTokenExpMillis = accessTokenExpMillis;
@@ -82,6 +86,7 @@ public class TokenManager {
     }
 
     public Claims getTokenClaims(String token) {
+        token = token.trim();
         try {
             return Jwts.parserBuilder()
                     .setSigningKey(key).build()
@@ -93,14 +98,20 @@ public class TokenManager {
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parserBuilder()
-                    .setSigningKey(key).build()
-                    .parseClaimsJws(token);
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            if (redisUtil.hasKeyBlackList(token)){
+                throw new BusinessException(ErrorCode.NOT_VALID_TOKEN);
+            }
             return true;
-        } catch (MalformedJwtException e) {
-            log.info("잘못된 jwt token", e);
+        }catch (MalformedJwtException e) {
+            log.info("잘못된 JWT 토큰: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰: {}", e.getMessage());
+            throw new BusinessException(ErrorCode.TOKEN_EXPIRED);
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰: {}", e.getMessage());
         } catch (JwtException e) {
-            log.info("jwt token 검증 중 에러 발생", e);
+            log.info("JWT 검증 중 에러 발생: {}", e.getMessage());
         }
         return false;
     }
